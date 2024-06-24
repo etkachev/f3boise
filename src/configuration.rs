618c3d396e485221ae2d7998343562d888661ffe
etkachev <edwardtkachev@gmail.com>
2024-06-24 10:04:05 -0600

    feat: cleaning up config more

--- src/configuration.rs
+++ src/configuration.rs
@@ -48,7 +48,7 @@ impl DatabaseSettings {
             .username(&self.username)
             .password(self.password.expose_secret())
             .port(self.port)
-            .ssl_mode(PgSslMode::Prefer)
+            .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
