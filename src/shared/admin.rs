f205125a27231fa3b02628f82e7f99086883a1f5
etkachev <edwardtkachev@gmail.com>
2024-07-30 09:56:17 -0600

    feat: admins can edit bb

---
+++ src/shared/admin.rs
@@ -0,0 +1,13 @@
+/// get list of admin users (slack ids)
+pub fn admin_users() -> Vec<String> {
+    vec![
+        // backslash
+        String::from("U03SR452HL7"),
+        // stinger
+        String::from("U03T87KHRFE"),
+        // guac
+        String::from("U04SS5FQXQ9"),
+        // deepdish
+        String::from("U05CWQ1FSV8"),
+    ]
+}
